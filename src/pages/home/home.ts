3d104f29f98bd38aaf8b7a9913291c547531132c
bryant.lee@atalent.com <bryant.lee@atalent.com>
2017-09-20 18:00:42 +0800

    add

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  result: any;
+  constructor(public navCtrl: NavController,public http: HttpClient) {
 
   }
-
+  ngOnInit(): void {
+    const me = this;
+    // Make the HTTP request:
+    this.http.get('https://dev.atalent.xyz/api/wx_info/get_info.php').subscribe(data => {
+      // Read the result field from the JSON response.
+      me.result = data;
+    });
+  }
 }
